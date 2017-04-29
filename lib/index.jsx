7f19a142cc79360324443c25416090bcb2d126f5
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2017-04-29 23:34:43 +0200

    Add wrapper for outside events

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Dropdown from "./components/dropdown.jsx";
 
-ReactDOM.render(
-	React.createElement(Dropdown, {
-		label: "Miesiąc",
-		warningText: "Wybierz miesiąc",
-		options: [
-			{ name: "Styczeń", value: "january" },
-			{ name: "Luty", value: "february" },
-			{ name: "Marzec", value: "march" },
-			{ name: "Kwiecień", value: "april" },
-			{ name: "Maj", value: "may" },
-			{ name: "Czerwiec", value: "june" },
-			{ name: "Lipiec", value: "july" },
-			{ name: "Sierpień", value: "august" },
-			{ name: "Wrzesień", value: "september" },
-			{ name: "Październik", value: "october" },
-			{ name: "Listopad", value: "november" },
-			{ name: "Grudzień", value: "december" },
-		],
-	}),
-	document.querySelector("#app")
-);
+class Index extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return (
+			<div className="element-container">
+				{React.createElement(Dropdown, {
+					label: "Miesiąc",
+					warningText: "Wybierz miesiąc",
+					options: [
+						{ name: "Styczeń", value: "january" },
+						{ name: "Luty", value: "february" },
+						{ name: "Marzec", value: "march" },
+						{ name: "Kwiecień", value: "april" },
+						{ name: "Maj", value: "may" },
+						{ name: "Czerwiec", value: "june" },
+						{ name: "Lipiec", value: "july" },
+						{ name: "Sierpień", value: "august" },
+						{ name: "Wrzesień", value: "september" },
+						{ name: "Październik", value: "october" },
+						{ name: "Listopad", value: "november" },
+						{ name: "Grudzień", value: "december" },
+					],
+				})}
+
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Index />, document.querySelector("#container"));
