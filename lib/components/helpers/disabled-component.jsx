7eac683f34c5a1d092c040de4ea22cf4c467cfe4
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2017-05-01 01:37:50 +0200

    Add DisabledComponent

---
+++ lib/components/helpers/disabled-component.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+const DisabledComponent = props => {
+	let value_name = null;
+	if (props.currentValue !== undefined)
+		value_name = props.currentValue.option.name;
+	return (
+		<ul className="select-container">
+			<li className="single">{value_name}</li>
+		</ul>
+	);
+};
+
+export default DisabledComponent;
